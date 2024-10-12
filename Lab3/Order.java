58d94f8b27bac0e98540fc0bb33ee83e1cbcf98a
Oleny <Olenydev@gmail.com>
2024-10-12 14:11:43 +0700

    lab3

---
+++ Lab3/Order.java
@@ -0,0 +1,48 @@
+package Lab3;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Order {
+    private OrderItem[] items;
+
+    public Order(OrderItem[] items) {
+        this.items = items;
+    }
+
+    public double cost() {
+        double total = 0;
+        for (OrderItem item : items) {
+            total += item.cost();
+        }
+        return total;
+    }
+
+    // using binary search approach
+    public boolean contains(Product p) {
+        for (OrderItem item : items) {
+            if (item.contain(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // get all products based on the given type using linear
+    //search
+    public Product[] filter(String type) {
+        List<Product> filtered = new ArrayList<Product>();
+        for (OrderItem item : items) {
+            if(item.containType(type)) filtered.add(item.getP());
+
+        }
+        Product[]result = new Product[filtered.size()];
+        result = filtered.toArray(result);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Cost: " + this.cost();
+    }
+}
---
+++ Lab3/Product.java
@@ -0,0 +1,31 @@
+package Lab3;
+
+public class Product {
+    private String id;
+    private String name;
+    private double price;
+    private String type;
+
+    public Product(String id, String name, double price, String type) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        if (this == null){
+            return "null";
+        }
+        return "ID: " + id + ", Name: " + name + ", Price: " + price + ", Type: " + type;
+    }
+}
