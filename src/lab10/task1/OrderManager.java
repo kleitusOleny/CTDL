ed7b5a4e0f16c5632803a68e5b957b2bd3af265d
Oleny KleiTus <olenyDev@gmail.com>
2024-12-14 13:41:44 +0700

    Lab10 - update Task 4.11->14 - with Java 8

--- src/lab10/task1/OrderManager.java
+++ src/lab10/task1/OrderManager.java
@@ -11,35 +11,36 @@ public class OrderManager {
         this.orders = orders;
     }
     
-    public Product maxProduct(){
+    public Product maxProduct() {
         Product product = orders.getFirst().getMaxProduct();
-        for (int  i = 1; i < orders.size(); i++){
-                product = product.ProductHigherPrice(orders.get(i).getMaxProduct());
+        for (int i = 1; i < orders.size(); i++) {
+            product = product.ProductHigherPrice(orders.get(i).getMaxProduct());
         }
         return product;
     }
     
-    public Optional<Product> maxProductJava8(){
-        return orders.stream().flatMap(order -> order.getItems().stream().map(OrderItem::getItem)).max((o1, o2) -> Integer.compare(o1.getPrice(),o2.getPrice()));
+    public Optional<Product> maxProductJava8() {
+        return orders.stream().flatMap(order -> order.getItems().stream().map(OrderItem::getItem)).max((o1, o2) -> Integer.compare(o1.getPrice(), o2.getPrice()));
     }
     
-    public HashMap<String, Integer> productTypesStatistics(){
+    public HashMap<String, Integer> productTypesStatistics() {
         HashMap<String, Integer> res = new HashMap<>();
         
-        for (Order order : orders){
+        for (Order order : orders) {
             HashMap<String, Integer> orderToMap = order.getProductType();
-            for (String key : orderToMap.keySet()){
-                    res.put(key,res.getOrDefault(key,0)+ orderToMap.getOrDefault(key,0));
+            for (String key : orderToMap.keySet()) {
+                res.put(key, res.getOrDefault(key, 0) + orderToMap.getOrDefault(key, 0));
             }
         }
         return res;
     }
     
-    public Map<String, Integer> productTypesStatisticsJava8(){
-        return orders.stream().flatMap(order -> order.getItems().stream()).collect(Collectors.toMap(o -> o.getItem().getType(), OrderItem::getAmount,Integer::sum));
+    public Map<String, Integer> productTypesStatisticsJava8() {
+        return orders.stream().flatMap(order -> order.getItems().stream())
+                .collect(Collectors.toMap(o -> o.getItem().getType(), OrderItem::getAmount, Integer::sum));
     }
     
-    public TreeSet<Order>ordersByCost(){
+    public TreeSet<Order> ordersByCost() {
         Comparator<Order> c = new Comparator<Order>() {
             @Override
             public int compare(Order o1, Order o2) {
@@ -47,26 +48,26 @@ public class OrderManager {
             }
             
         };
-        Comparator<Order> com = (o1,o2) -> o1.totalPrice() - o2.totalPrice();
+        Comparator<Order> com = (o1, o2) -> o1.totalPrice() - o2.totalPrice();
         TreeSet<Order> result = new TreeSet<>(c);
         result.addAll(orders);
         return result;
     }
     
-    public Set<Order>ordersByCostJava8(){
+    public Set<Order> ordersByCostJava8() {
         Set<Order> res = new TreeSet<Order>(((o1, o2) -> (o1.totalPrice() - o2.totalPrice() != 0 ? o1.totalPrice() - o2.totalPrice() : o1.getDate().compareTo(o2.getDate()))));
         res.addAll(orders);
         return res;
     }
     
     public static void main(String[] args) {
-        Product p1 = new Product("A","T",270000,LocalDate.of(2024,5,1));
-        Product p2 = new Product("B","T",150000,LocalDate.of(2024,5,2));
-        Product p3 = new Product("C","V",200000,LocalDate.of(2024,5,3));
-        Product p4 = new Product("D","V",250000,LocalDate.of(2024,5,3));
+        Product p1 = new Product("A", "T", 270000, LocalDate.of(2024, 5, 1));
+        Product p2 = new Product("B", "T", 150000, LocalDate.of(2024, 5, 2));
+        Product p3 = new Product("C", "V", 200000, LocalDate.of(2024, 5, 3));
+        Product p4 = new Product("D", "V", 250000, LocalDate.of(2024, 5, 3));
         
-        OrderItem orderItem = new OrderItem(p1,10);
-        OrderItem orderItem1 = new OrderItem(p2,20);
+        OrderItem orderItem = new OrderItem(p1, 10);
+        OrderItem orderItem1 = new OrderItem(p2, 20);
         OrderItem orderItem2 = new OrderItem(p3, 5);
         OrderItem orderItem3 = new OrderItem(p4, 15);
         
@@ -78,8 +79,8 @@ public class OrderManager {
         List<OrderItem> orderItemList1 = new ArrayList<>();
         orderItemList1.add(orderItem3);
         
-        Order order = new Order("001","Vinh","V",LocalDate.of(2024,5,1),orderItemList);
-        Order order1 = new Order("002","T","V",LocalDate.of(2024,5,3),orderItemList1);
+        Order order = new Order("001", "Vinh", "V", LocalDate.of(2024, 5, 1), orderItemList);
+        Order order1 = new Order("002", "T", "V", LocalDate.of(2024, 5, 3), orderItemList1);
         
         List<Order> orderList = new ArrayList<>();
         orderList.add(order);
@@ -90,7 +91,7 @@ public class OrderManager {
         System.out.println(orderManager.maxProductJava8());
         
         System.out.println(orderManager.productTypesStatisticsJava8());
-
+        
         System.out.println(orderManager.ordersByCostJava8());
         
     }
